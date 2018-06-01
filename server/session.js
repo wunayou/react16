9b5185de727dd229be96616646d6021c7c43aa85
yangli <wunayou>
2018-06-01 13:23:55 +0800

    代码

--- server/session.js
+++ server/session.js
@@ -18,12 +18,39 @@ app.use(session({
  * session 是把ID 当COOKIES当在浏览器
  * 然后可以根据这个ID 找到相对应的数据
  * store 是可以自定义的数据
+ * 
+ * 为什么要引用session
+ * 解决了什么问题
+ * 解决这个问题又产生了什么新的问题
+ * 如何更加合理的解决这个问题
+ * 如果把信息全部放在cookies，信息过大，cookies有限制
+ * 安全问题
+ * 经验不足 不知道还有其它原因 要使用session
+ * session 的解决方案是什么
+ * 1、cookies 保存session_id
+ * 2、通过session_id 得到查询信息
+ * 
+ * 产生的问题
+ * session的数据存在那里 内存 还是数据库
+ * express-session 他是保存在内存里面的
+ * 只要重启node服务，这些数据就没有了，
+ * 
+ * 更新问题
+ * 
+ * 删除问题
+ * 
+ * 创建问题
+ * 
+ * https://segmentfault.com/q/1010000010862121
+ * 
+ * uuid的使用
 */
  
 
 app.get('/awesome', function(req, res){
      
     console.log(req.cookies)
+    console.log(req.sessionID)
     if(req.session.lastPage) {
         console.log('Last page was: ' + req.session.lastPage + ".");   
     }   
@@ -33,6 +60,7 @@ app.get('/awesome', function(req, res){
  
 app.get('/radical', function(req, res){
     console.log(req.cookies)
+    console.log(req.sessionID)
     if (req.session.lastPage) {
         console.log('Last page was: ' + req.session.lastPage + ".");   
     }
@@ -42,6 +70,7 @@ app.get('/radical', function(req, res){
  
 app.get('/tubular', function(req, res){
     console.log(req.cookies)
+    console.log(req.sessionID)
     if (req.session.lastPage){
         console.log("Last page was: " + req.session.lastPage + ".");   
     }
