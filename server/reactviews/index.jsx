d3c5ef776201ac5460f2a66c6f678c1318c8bead
wunayou <574612524@qq.com>
2018-06-02 09:10:26 +0800

    实用 react epress  views

---
+++ server/reactviews/index.jsx
@@ -0,0 +1,34 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+var Layout = require('./layout');
+
+// Contrived example to show how one might use Flow type annotations
+function countTo(n: number): string {
+  var a = [];
+  for (var i = 0; i < n; i++) {
+    a.push(i + 1);
+  }
+  return a.join(', ');
+}
+
+class Index extends React.Component {
+  render() {
+    return (
+      <Layout title={this.props.title}>
+        <h1>{this.props.title}</h1>
+        <p>Welcome to {this.props.title}</p>
+        <p>
+          I can count to 10:
+          {this.props.foo.bar}
+          {countTo(10)}
+        </p>
+      </Layout>
+    );
+  }
+}
+
+Index.propTypes = {
+  title: PropTypes.string,
+};
+
+module.exports = Index;
